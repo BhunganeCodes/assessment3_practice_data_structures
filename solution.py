018c4c383aaab411fa3d6907d1b1c662bad42f0d
BhunganeCodes <thamsanqahadebe98@gmail.com>
2026-02-03 13:15:21 +0200

    solved: frequency map

--- solution.py
+++ solution.py
@@ -5,12 +5,23 @@ def unique_sorted(lst: list[int]) -> list[int]:
         return []
     return sorted(lst)
 
+
 def rotate_list(lst: list, k: int) -> list:
     if not lst:
         return []
     for i in range(abs(k)):
         lst.insert(0, lst.pop())
     return lst
+
+
+def char_frequency(text: str) -> dict:
+    res = {}
+    for char in text:
+        if char in res:
+            res[char] += 1
+        else:
+            res[char] = 1
+    return res
 
 if __name__ == "__main__":
     pass
