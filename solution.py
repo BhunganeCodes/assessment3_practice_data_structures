ee6f74a4457a4d1d6e7c8f015220a01704cba58a
BhunganeCodes <thamsanqahadebe98@gmail.com>
2026-02-03 10:53:24 +0200

    solved: unique sorted

--- solution.py
+++ solution.py
@@ -1,1 +1,12 @@
-# write your code here
+
+
+def unique_sorted(lst: list[int]) -> list[int]:
+    if not lst:
+        return []
+    return sorted(lst)
+
+def rotate_list(lst):
+    pass
+
+if __name__ == "__main__":
+    pass
